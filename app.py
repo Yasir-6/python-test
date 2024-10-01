258986cb08164fab0beded674711393d22ceb111
Yasir-6 <myasir24206@gmail.com>
2024-10-01 00:09:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
-import socket
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World! This is my Project and Successfully ........!!! I\'m host %s' % socket.gethostname()
+    return 'Hello, World!'
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run()
+
 
 
